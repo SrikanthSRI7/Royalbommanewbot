d7b80cdf10c482a3519cde97e6c1f32b1e7062f3
SrikanthSRI7 <154247211+SrikanthSRI7@users.noreply.github.com>
2025-11-16 18:54:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -224,7 +224,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/ROYALBOMMASR3">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/ROYALBOMMASR3">[ ROYALBOMMASR3 ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
