a6bd9406ca0875b4b2546ba4172ef260668aade9
SrikanthSRI7 <154247211+SrikanthSRI7@users.noreply.github.com>
2025-11-16 19:10:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://srikanthreddycmd77:BZd72iOaOfcXrvlP@cluster0.hquo2bt.mongodb.net/?appName=Cluster0")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://luckyyy2357_db_user:0tTjknoyPKBSvqJK@cluster0.k8s1rqv.mongodb.net/")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
